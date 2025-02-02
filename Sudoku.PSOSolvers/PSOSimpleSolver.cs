e573af4e09bde56a146b83fa8504b9c1532289ca
amine833 <aminegharbi7@gmail.com>
2025-02-03 00:18:54 +0100

    Initialisation du solveur PSO vide

---
+++ Sudoku.PSOSolvers/PSOSimpleSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.PSOSolvers;
+
+public class PSOsimpleSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
