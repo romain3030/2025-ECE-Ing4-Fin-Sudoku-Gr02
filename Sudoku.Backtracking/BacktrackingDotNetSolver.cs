852d6a824ebec67cab2c61831e3b6e90444c0ace
jeromefossourier <104997655+jeromefossourier@users.noreply.github.com>
2025-02-03 22:45:12 +0100

    code V1 PSO - easy
    le code fonctionne pour la premier sudoku mais pas pour les autres (medium, hard)

--- Sudoku.Backtracking/BacktrackingDotNetSolver.cs
+++ Sudoku.Backtracking/BacktrackingDotNetSolver.cs
@@ -1,93 +1,192 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Sudoku.Shared;
 
-namespace Sudoku.Backtracking
+namespace Sudoku.PSO
 {
-	public class BacktrackingDotNetSolver : ISudokuSolver
-	{
+    public class PSOSolver : ISudokuSolver
+    {
+        private const int PopulationSize = 100;
+        private const int MaxIterations = 1000;
+        private const double InertiaWeight = 0.729;
+        private const double CognitiveWeight = 1.494;
+        private const double SocialWeight = 1.494;
+        private Random _random = new Random();
 
-		/// <summary>
-		/// Solves the given Sudoku grid using a backtracking algorithm.
-		/// </summary>
-		/// <param name="s">The Sudoku grid to be solved.</param>
-		/// <returns>
-		/// The solved Sudoku grid.
-		/// </returns>
-		public SudokuGrid Solve(SudokuGrid s)
-		{
-			//launch the solver
-			callCount = 0;
-			Search(s, 0, 0);
-			Console.WriteLine("BacktrackingDotNetSolver: " + callCount + " search calls");
-			return s;
-		}
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            List<Particle> swarm = InitializeSwarm(s);
+            Particle globalBest = GetBestParticle(swarm);
 
-		private int callCount = 0;
+            for (int iter = 0; iter < MaxIterations; iter++)
+            {
+                foreach (var particle in swarm)
+                {
+                    UpdateVelocity(particle, globalBest);
+                    UpdatePosition(particle, s);
+                    UpdateBestPosition(particle);
+                }
 
-		/// <summary>
-		/// Recursively searches for a solution to a Sudoku grid by backtracking
-		/// </summary>
-		/// <param name="s">The Sudoku grid to search</param>
-		/// <param name="row">The current row being checked</param>
-		/// <param name="col">The current column being checked</param>
-		/// <returns>True if a solution is found, false otherwise</returns>
-		private bool Search(SudokuGrid s, int row, int col)
-		{
-			callCount++;
-			//pass to the next row if all the cells in the column are checked     
-			if (col == 9)
-			{
-				col = 0; ++row;
-				if (row == 9) return true;
-			}
-			//check if the cell is filled
-			if (s.Cells[row, col] != 0)
-				return Search(s, row, col + 1);
-			//implement the good value
-			for (int v = 1; v <= 9; v++)
-			{
-				if (IsValid(s, row, col, v))
-				{
-					s.Cells[row, col] = v;
-					if (Search(s, row, col + 1)) return true;
-					else s.Cells[row, col] = 0;
-				}
-			}
-			return false;
-		}
+                Particle bestInSwarm = GetBestParticle(swarm);
+                if (bestInSwarm.BestFitness < globalBest.BestFitness)
+                {
+                    globalBest = bestInSwarm;
+                }
 
+                if (globalBest.BestFitness == 0) // Found a valid solution
+                {
+                    break;
+                }
+            }
 
-		/// <summary>
-		/// Check if a value is valid to be placed in a specific cell of a Sudoku grid.
-		/// </summary>
-		/// <param name="s">The Sudoku grid to check against.</param>
-		/// <param name="row">The row index of the cell to check.</param>
-		/// <param name="col">The column index of the cell to check.</param>
-		/// <param name="val">The value to be checked for validity.</param>
-		/// <returns>
-		/// True if the value is valid to be placed in the cell, false otherwise.
-		/// </returns>
-		private bool IsValid(SudokuGrid s, int row, int col, int val)
-		{
-			//check if value is present in column
-			for (int r = 0; r < 9; r++)
-				if (s.Cells[r, col] == val) return false;
+            return globalBest.BestPosition;
+        }
 
-			//check if value is present in the row
-			for (int c = 0; c < 9; c++)
-				if (s.Cells[row, c] == val) return false;
+        private List<Particle> InitializeSwarm(SudokuGrid s)
+        {
+            List<Particle> swarm = new List<Particle>();
+            for (int i = 0; i < PopulationSize; i++)
+            {
+                swarm.Add(new Particle(s, _random));
+            }
+            return swarm;
+        }
 
-			//check for the value in the 3 X 3 block
-			int i = row / 3; int j = col / 3;
-			for (int a = 0; a < 3; a++)
-				for (int b = 0; b < 3; b++)
-					if (val == s.Cells[3 * i + a, 3 * j + b]) return false;
+        private Particle GetBestParticle(List<Particle> swarm)
+        {
+            return swarm.OrderBy(p => p.BestFitness).First();
+        }
 
-			return true;
-		}
-	}
+        private void UpdateVelocity(Particle particle, Particle globalBest)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (particle.IsFixed[i, j]) continue;
+
+                    double r1 = _random.NextDouble();
+                    double r2 = _random.NextDouble();
+                    particle.Velocity[i, j] = InertiaWeight * particle.Velocity[i, j] +
+                                              CognitiveWeight * r1 * (particle.BestPosition.Cells[i, j] - particle.CurrentPosition.Cells[i, j]) +
+                                              SocialWeight * r2 * (globalBest.BestPosition.Cells[i, j] - particle.CurrentPosition.Cells[i, j]);
+                }
+            }
+        }
+
+        private void UpdatePosition(Particle particle, SudokuGrid s)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!particle.IsFixed[i, j])
+                    {
+                        int newValue = (int)Math.Round(particle.CurrentPosition.Cells[i, j] + particle.Velocity[i, j]);
+                        particle.CurrentPosition.Cells[i, j] = Math.Clamp(newValue, 1, 9);
+                    }
+                }
+            }
+            particle.CurrentFitness = ComputeFitness(particle.CurrentPosition);
+        }
+
+        private void UpdateBestPosition(Particle particle)
+        {
+            if (particle.CurrentFitness < particle.BestFitness)
+            {
+                particle.BestPosition = new SudokuGrid { Cells = (int[,])particle.CurrentPosition.Cells.Clone() };
+                particle.BestFitness = particle.CurrentFitness;
+            }
+        }
+
+        private int ComputeFitness(SudokuGrid s)
+        {
+            int conflicts = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                conflicts += CountConflicts(s, i, true);
+                conflicts += CountConflicts(s, i, false);
+            }
+            return conflicts;
+        }
+
+        private int CountConflicts(SudokuGrid s, int index, bool isRow)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            int conflicts = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                int value = isRow ? s.Cells[index, i] : s.Cells[i, index];
+                if (value != 0 && !seen.Add(value))
+                {
+                    conflicts++;
+                }
+            }
+            return conflicts;
+        }
+    }
+
+    public class Particle
+    {
+        public SudokuGrid CurrentPosition { get; set; }
+        public SudokuGrid BestPosition { get; set; }
+        public double[,] Velocity { get; set; }
+        public int CurrentFitness { get; set; }
+        public int BestFitness { get; set; }
+        public bool[,] IsFixed { get; set; }
+
+        public Particle(SudokuGrid initialGrid, Random random)
+        {
+            CurrentPosition = new SudokuGrid { Cells = (int[,])initialGrid.Cells.Clone() };
+            BestPosition = new SudokuGrid { Cells = (int[,])initialGrid.Cells.Clone() };
+            Velocity = new double[9, 9];
+            IsFixed = new bool[9, 9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (CurrentPosition.Cells[i, j] != 0)
+                    {
+                        IsFixed[i, j] = true;
+                    }
+                    else
+                    {
+                        CurrentPosition.Cells[i, j] = random.Next(1, 10);
+                    }
+                    Velocity[i, j] = random.NextDouble() * 2 - 1;
+                }
+            }
+
+            CurrentFitness = ComputeFitness();
+            BestFitness = CurrentFitness;
+        }
+
+        private int ComputeFitness()
+        {
+            int conflicts = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                conflicts += CountConflicts(i, true);
+                conflicts += CountConflicts(i, false);
+            }
+            return conflicts;
+        }
+
+        private int CountConflicts(int index, bool isRow)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            int conflicts = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                int value = isRow ? CurrentPosition.Cells[index, i] : CurrentPosition.Cells[i, index];
+                if (value != 0 && !seen.Add(value))
+                {
+                    conflicts++;
+                }
+            }
+            return conflicts;
+        }
+    }
 }
--- Sudoku.PSOSolvers/PSOSimpleSolver.cs
+++ Sudoku.PSOSolvers/PSOSimpleSolver.cs
@@ -1,11 +1,183 @@
-﻿using Sudoku.Shared;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sudoku.Shared;
 
-namespace Sudoku.PSOSolvers;
-
-public class PSOsimpleSolver : ISudokuSolver
+namespace Sudoku.PSOSolvers
 {
-    public SudokuGrid Solve(SudokuGrid s)
+    public class PSOSimpleSolver : ISudokuSolver
     {
-        return s;
+        private const int PopulationSize = 50;
+        private const int MaxIterations = 1000;
+        private static readonly Random RandomGen = new Random();
+
+        public SudokuGrid Solve(SudokuGrid grid)
+        {
+            List<Particle> swarm = InitializeSwarm(grid);
+            Particle bestGlobal = swarm.OrderBy(p => p.Fitness).First();
+
+            for (int iter = 0; iter < MaxIterations; iter++)
+            {
+                foreach (var particle in swarm)
+                {
+                    particle.UpdateVelocity(bestGlobal);
+                    particle.UpdatePosition(grid);
+                    particle.EvaluateFitness();
+
+                    if (particle.Fitness == 0)
+                        return particle.ToSudokuGrid();
+                }
+
+                bestGlobal = swarm.OrderBy(p => p.Fitness).First();
+                if (bestGlobal.Fitness == 0)
+                    break;
+            }
+
+            Console.WriteLine("PSOSimpleSolver: " + MaxIterations + " iterations");
+            return bestGlobal.ToSudokuGrid();
+        }
+
+        private List<Particle> InitializeSwarm(SudokuGrid grid)
+        {
+            List<Particle> swarm = new List<Particle>();
+            for (int i = 0; i < PopulationSize; i++)
+            {
+                swarm.Add(new Particle(grid));
+            }
+            return swarm;
+        }
+    }
+
+    public class Particle
+    {
+        public SudokuGrid Position { get; set; }
+        public SudokuGrid BestPosition { get; set; }
+        public double Fitness { get; set; }
+        public double[,] Velocity { get; set; }
+        private bool[,] IsImmutable { get; set; }
+        private static readonly Random Random = new Random();
+
+        public Particle(SudokuGrid grid)
+        {
+            Position = new SudokuGrid();
+            BestPosition = new SudokuGrid();
+            Velocity = new double[9, 9];
+            IsImmutable = new bool[9, 9];
+            Initialize(grid);
+        }
+
+        private void Initialize(SudokuGrid grid)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                var missing = Enumerable.Range(1, 9).Except(GetRowValues(grid, i)).ToList();
+                for (int j = 0; j < 9; j++)
+                {
+                    if (grid.Cells[i, j] == 0)
+                    {
+                        Position.Cells[i, j] = missing.OrderBy(x => Random.Next()).First();
+                        missing.Remove(Position.Cells[i, j]);
+                        IsImmutable[i, j] = false;
+                    }
+                    else
+                    {
+                        Position.Cells[i, j] = grid.Cells[i, j];
+                        IsImmutable[i, j] = true;
+                    }
+                    BestPosition.Cells[i, j] = Position.Cells[i, j];
+                }
+            }
+            EvaluateFitness();
+        }
+
+        private IEnumerable<int> GetRowValues(SudokuGrid grid, int row)
+        {
+            return Enumerable.Range(0, 9).Select(col => grid.Cells[row, col]);
+        }
+
+        public void UpdateVelocity(Particle bestGlobal)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!IsImmutable[i, j])
+                    {
+                        Velocity[i, j] = 0.3 * Velocity[i, j]
+                            + 1.5 * Random.NextDouble() * (BestPosition.Cells[i, j] - Position.Cells[i, j])
+                            + 1.5 * Random.NextDouble() * (bestGlobal.Position.Cells[i, j] - Position.Cells[i, j]);
+                    }
+                }
+            }
+        }
+
+        public void UpdatePosition(SudokuGrid grid)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!IsImmutable[i, j])
+                    {
+                        int newValue = (int)Math.Round(Position.Cells[i, j] + Velocity[i, j]);
+                        newValue = Math.Max(1, Math.Min(9, newValue));
+
+                        if (IsValidValue(grid, i, j, newValue))
+                        {
+                            Position.Cells[i, j] = newValue;
+                        }
+                        else
+                        {
+                            var possibleValues = Enumerable.Range(1, 9).Where(val => IsValidValue(Position, i, j, val)).ToList();
+                            if (possibleValues.Any())
+                                Position.Cells[i, j] = possibleValues[Random.Next(possibleValues.Count)];
+                        }
+                    }
+                }
+            }
+            EvaluateFitness();
+        }
+
+        public void EvaluateFitness()
+        {
+            Fitness = CalculateConflicts(Position);
+        }
+
+        private double CalculateConflicts(SudokuGrid grid)
+        {
+            double conflicts = 0;
+            
+            for (int i = 0; i < 9; i++)
+            {
+                conflicts += 9 - GetRowValues(grid, i).Distinct().Count();
+                conflicts += 9 - Enumerable.Range(0, 9).Select(col => grid.Cells[col, i]).Distinct().Count();
+            }
+            
+            for (int box = 0; box < 9; box++)
+            {
+                int rowStart = (box / 3) * 3;
+                int colStart = (box % 3) * 3;
+                conflicts += 9 - Enumerable.Range(0, 9)
+                    .Select(index => grid.Cells[rowStart + index / 3, colStart + index % 3])
+                    .Distinct().Count();
+            }
+
+            return conflicts;
+        }
+
+        private bool IsValidValue(SudokuGrid grid, int row, int col, int value)
+        {
+            if (Enumerable.Range(0, 9).Any(i => grid.Cells[row, i] == value || grid.Cells[i, col] == value))
+                return false;
+
+            int regionRow = (row / 3) * 3;
+            int regionCol = (col / 3) * 3;
+            return !Enumerable.Range(0, 9).Any(i => grid.Cells[regionRow + i / 3, regionCol + i % 3] == value);
+        }
+
+        public SudokuGrid ToSudokuGrid()
+        {
+            return Position;
+        }
     }
 }
